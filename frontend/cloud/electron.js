a7d71e08b4235d9063b7a78ed8e3249bdc9e0816
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2026-01-06 15:39:01 -0500

    more electron configuraitions

--- frontend/cloud/electron.js
+++ frontend/cloud/electron.js
@@ -3,14 +3,13 @@ const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1000,
-    height: 700,
+    fullscreen: true,
+    autoHideMenuBar: true
   });
 
   // DEV vs PROD
   if (!app.isPackaged) {
     win.loadURL("http://localhost:3000");
-    win.webContents.openDevTools();
   } else {
     win.loadFile(path.join(__dirname, "build", "index.html"));
   }
